6220538490223880b67f61670b50efdb33df9df8
Oliver Rodriguez <oliver.rdz@gmail.com>
2025-07-19 17:58:53 +0100

    Added setup function so user doesn't deal with DB operations

--- api.py
+++ api.py
@@ -1,50 +1,97 @@
 import sqlite3
 
+def setup(db_name="pythonaire.db"):
+    """
+    Creates db and tables
+    """
+    db = DB()
+    db.create_tables(db_name)
+    db.close()
+
 class DB:
+    """
+    New database 
+    """
     def connect(self, db_name):
+        """
+        Connects to database db_name
+        """
         try:
             self.conn = sqlite3.connect(db_name)
             # Needs to be added according to ChatGPT 4o
             self.conn.execute("PRAGMA foreign_Keys = ON;")
             self.conn.row_factory = sqlite3.Row
             print(f"\nConnected to {db_name}.")
-            return self.conn
+            self.cursor = self.conn.cursor()
+            return 1
         except sqlite3.Error as e:
             print(f"\nFailed to connect to {db_name}.", e)
             return 0
 
+    def create_tables(self, db_name):
+        """
+        Creates all required tables
+
+        Pending:
+        * Check if tables already exist
+        """
+        if not self.connect(db_name):
+            print(f"Can't connect to database {db_name}")
+            return 0
+        # Create table account_type:
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS account_type (
+        type_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        type_name varchar(100),
+        type varchar(100)
+        )
+        """)
+        print("Table account_type created")
+
+        # Create table accounts
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS accounts(
+        account_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name text NOT NULL,
+        type_id integer NOT NULL,
+        total float NOT NULL,
+        FOREIGN KEY (type_id) REFERENCES account_type(type_id)
+        )
+        """)
+        print("Table accounts created")
+
+        # Create table transactions
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS transactions (
+        transaction_id integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+        date datetime DEFAULT CURRENT_TIMESTAMP,
+        account_id integer NOT NULL,
+        ammount_added float NOT NULL,
+        notes text,
+        FOREIGN KEY (account_id) REFERENCES accounts(account_id)
+        )
+        """)
+        print("Table transactions created")
+
+
     def close(self):
+        """
+        Closes database
+        """
         self.conn.close()
         print(f"\nDatabase closed.\n")
 
-def read_account_types(conn):
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
-    cursor.execute("""
-        SELECT * FROM account_type
-    """)
-    rows = cursor.fetchall()
-    account_types = [dict(row) for row in rows]
-    return account_types
 
-def read_accounts(conn):
-    cursor.execute("""
-        SELECT accounts.name, account_type.type, accounts.total
-        FROM accounts
-        JOIN account_type ON accounts.account_id = account_type.type_id
-    """)
-    rows = cursor.fetchall()
-    accounts = [dict(row) for row in rows]
-    return accounts
 
 if __name__ == "__main__":
     import api 
 
     # Create database
     db_name = "sqlite.db"
-    db = api.DB()
-    conn = db.connect(db_name)
-    db.close()
+    setup()
+    #db = api.DB()
+    #db.create_tables(db_name)
+    #db.close()
 
     # Delete database
     if 0:
